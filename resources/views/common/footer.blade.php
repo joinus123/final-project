aaace3e03e7bc6ccba3059abe7b6e40a958b6757
Awais Ahmed <ahmed.awais@technado.pk>
2021-05-19 06:12:31 +0500

    first commit

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,85 @@
+<footer>
+    <div class="w-100 bg-color5 pt-100 pb-10 position-relative">
+        <div class="particles-js" id="prtcl5"></div>
+        <div class="container">
+            <div class="footer-data w-100">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-lg-3">
+                        <div class="widget w-100">
+                            <div class="logo w-100">
+                                <h1 class="mb-0"><a href="index.html" title="Home"><img class="img-fluid" src="assets/images/logo-3.png" alt="Logo" srcset="assets/images/logo-3.png"></a></h1>
+                            </div><!-- Logo -->
+                            <p class="mb-0">We Supply You A Complete List Of Home Contractors - Any Business That Has Something To Do With A Home.</p>
+                            <div class="social-links2 d-inline-block">
+                                <a href="javascript:void(0);" title="Facebook" target="_blank"><i class="flaticon-facebook"></i></a>
+                                <a href="javascript:void(0);" title="Twitter" target="_blank"><i class="flaticon-twitter"></i></a>
+                                <a href="javascript:void(0);" title="Instagram" target="_blank"><i class="fab fa-instagram"></i></a>
+                                <a href="javascript:void(0);" title="Google Plus" target="_blank"><i class="flaticon-google-plus-logo"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-lg-9">
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4 col-lg-4">
+                                <div class="widget w-100">
+                                    <h3>About Us</h3>
+                                    <ul class="mb-0 list-unstyled w-100">
+                                        <li><a href="#" title="">About Us</a></li>
+                                        <li><a href="#" title="">Home Owner</a></li>
+                                        <li><a href="#" title="">Contractors</a></li>
+                                        <li><a href="#" title="">Contact Us</a></li>
+                                        <li><a href="#" title="">Blog</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-lg-4">
+                                <div class="widget w-100">
+                                    <h3>Quick Links</h3>
+                                    <ul class="mb-0 list-unstyled w-100">
+                                        <li><a href="#" title="">HOME OWNER LIBRARY OF HELP</a></li>
+                                        <li><a href="#" title="">Let's find you a contractor</a></li>
+                                        <li><a href="#" title="">Here's  your results by Zip code</a></li>
+                                        <li><a href="#" title="">Major category of contractors</a></li>
+                                        <li><a href="#" title="">Specialties or areas of Expertise</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-lg-4">
+                                <div class="widget w-100">
+                                    <h3>More Links</h3>
+                                    <ul class="mb-0 list-unstyled w-100">
+                                        <li><a href="#" title="">Contractors Get Listed In Our Directory</a></li>
+                                        <li><a href="#" title="">What does a page look like for me?</a></li>
+                                        <li><a href="#" title="">Pay to become a member page</a></li>
+                                        <li><a href="#" title="">Fill in the application for membership</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- Footer Data -->
+        </div>
+    </div>
+</footer>
+<div class="copyright w-100 text-center bg-color6 position-relative">
+    <div class="container">
+        <p class="mb-0">Copyright by <a href="index.html" title="">@The House Facts</a>. All Rights Reserved</p>
+    </div>
+</div><!-- Copyright -->
+</main><!-- Main Wrapper -->
+
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/js/popper.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/wow.min.js')}}"></script>
+<script src="{{asset('assets/js/counterup.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.fancybox.min.js')}}"></script>
+<script src="{{asset('assets/js/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('assets/js/slick.min.js')}}"></script>
+<script src="{{asset('assets/js/particles.min.js')}}"></script>
+<script src="{{asset('assets/js/particle-int.js')}}"></script>
+<script src="{{asset('assets/js/custom-scripts.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link rel="icon" href="assets/images/favicon-1.png" sizes="35x35" type="image/png">
+        <title>The House facts - Home Contractors Directory</title>
+
+        <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/perfect-scrollbar.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/color.css') }}">
+    </head>
+    <body>
+        <main>
+            <header class="stick style1 w-100">
+                <div class="topbar bg-color1 w-100">
+                    <div class="container">
+                        <div class="topbar-inner d-flex flex-wrap justify-content-between align-items-center w-100">
+                            <ul class="topbar-info-list mb-0 list-unstyled top_flex">
+                                <li><i class="thm-clr fas fa-map-marker-alt"></i>525B Winola Rd, Clarks Summit PA 18411-1937, United States</li>
+                                <li><i class="thm-clr far fa-envelope-open"></i>Email: <a href="javascript:void(0);" title="">bmac@thehousefacts.com</a></li>
+                            </ul>
+                            <!-- <ul class="topbar-links mb-0 list-unstyled d-inline-flex">
+                                <li><a href="javascript:void(0);" title="">Careers</a></li>
+                                <li><a href="javascript:void(0);" title="">Help Desk</a></li>
+                                <li><a href="javascript:void(0);" title="">Login</a></li>
+                            </ul> -->
+                        </div>
+                    </div>
+                </div><!-- Topbar -->
+                <div class="logo-info-bar-wrap w-100">
+                    <div class="container">
+                        <div class="logo-info-bar-inner w-100 d-flex flex-wrap justify-content-between align-items-center">
+                            <div class="logo-social d-inline-flex flex-wrap justify-content-between align-items-center">
+                                <div class="logo"><h1 class="mb-0"><a href="3" title="Home"><img class="img-fluid" src="assets/images/logo-3.png" alt="Logo" srcset="assets/images/logo-3.png"></a></h1></div><!-- Logo -->
+                                <div class="social-links">
+                                    <a href="javascript:void(0);" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="javascript:void(0);" title="Twitter" target="_blank"><i class="fab fa-twitter"></i></a>
+                                    <a href="javascript:void(0);" title="Skype" target="_blank"><i class="fab fa-skype"></i></a>
+                                    <a href="javascript:void(0);" title="Vimeo" target="_blank"><i class="fab fa-vimeo-v"></i></a>
+                                </div>
+                            </div>
+                            <div class="top-info-wrap d-inline-flex flex-wrap justify-content-between align-items-center">
+                                <div class="call-us">
+                                    <i class="thm-clr flaticon-phone-call"></i>
+                                    <span>24/7 Phone Services</span>
+                                    <strong>+1 570 586 6062</strong>
+                                </div>
+                                <!-- <div class="add-cart">
+                                    <a href="cart.html" title="">
+                                        <i class="thm-bg fas fa-shopping-basket"></i>
+                                        Add to Cart
+                                        <span class="d-block">(Item: 02)</span>
+                                    </a>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- Logo Info Bar Wrap -->
+                <div class="menu-wrap">
+                    <div class="container">
+                        <nav class="d-flex justify-content-between align-items-center w-100 bg-color1">
+                            <div class="w-100">
+                                <ul class="mb-0 list-unstyled d-flex justify-content-center" >
+                                    <li ><a href="javascript:void(0);" title="">Home</a>
+                                    </li>
+                                    <li><a href="#" title="">About Us</a></li>
+                                    <li class="menu-item-has-children"><a href="javascript:void(0);" title="">Home Owner</a>
+                                        <ul class="mb-0 list-unstyled">
+                                            <li><a href="#" title="">HOME OWNER LIBRARY OF HELP</a></li>
+                                            <li><a href="#" title="">Let's find you a contractor</a></li>
+                                            <li class="menu-item-has-children"><a href="#" title="">Zip Code</a>
+                                                <ul class="mb-0 list-unstyled">
+                                                    <li><a href="#" title="">Here's  your results by Zip code</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="menu-item-has-children"><a href="javascript:void(0);" title="">Major category of contractors</a>
+                                                <ul class="mb-0 list-unstyled">
+                                                    <li><a href="#" title="">Heres your results</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="menu-item-has-children"><a href="#" title="">Specialties or areas of Expertise</a>
+                                                <ul class="mb-0 list-unstyled">
+                                                    <li><a href="#" title="">Results by Specialty category</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="#" title="">Gallery</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="javascript:void(0);" title="">Contractors</a>
+                                        <ul class="mb-0 list-unstyled">
+                                            <li class="menu-item-has-children"><a href="javascript:void(0);" title="">Contractors get listed in our directory</a>
+                                            </li>
+                                            <li><a href="#" title="">What does a page look like for me?</a></li>
+                                            <li><a href="#" title="">Pay to become a member page</a></li>
+                                            <li><a href="#" title="">Fill in the application for membership</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="javascript:void(0);" title="">Blog</a>
+                                    </li>
+                                    <li><a href="#" title="">Contact Us</a></li>
+                                </ul>
+                            </div>
+                            <!-- <div class="header-right-btns">
+                                <a class="get-quote" href="javascript:void(0);" title=""><i class="far fa-comments"></i>Get A Quote<i class="flaticon-arrow-pointing-to-right"></i></a>
+                            </div> -->
+                        </nav>
+                    </div>
+                </div><!-- Menu Wrap -->
+            </header><!-- Header -->
